97a8a2c1724d2e5f1cc44eb25ce7e14f4e006c97
Zachary Marion <zmarion@utk.edu>
2018-10-16 18:08:21 +1300

    trying to improve baseline_runs.sh

--- scripts/002_baseline_runs.sh
+++ scripts/002_baseline_runs.sh
@@ -59,104 +59,114 @@ if [ "$EVOLVING" = true ]; then printf NUM_COMMANDS=$((NUM_COMMANDS+1)); fi
 
 
 ### --- RUN THE PARAMETER COMBINATIONS --- ###
+
+for iters in `seq 6`; then
 #No genetic architectures, just additive genetic variance
-if [ "$NO_GENETICS" = true ]; then
-	
-	for i in `seq ${NUMREPS}`; do
-		./ARTs --courter --no-genetics -b ../../results/courter_unlinked_${i} --verbose --same-base -p 4
-		./ARTs --parent --no-genetics -b ../../results/parent_unlinked_${i} --verbose --same-base -p 4
-		./ARTs --courter --no-genetics --parent -b ../../results/parent-courter_unlinked_${i} --verbose --same-base -p 4
-		if [ "$INDEP_PREF" = true ]; then
-			./ARTs --courter --no-genetics --independent-pref -b ../../results/courter-pref-nogenetics_${i} --verbose --same-base -p 4
-			./ARTs --parent --no-genetics --independent-pref -b ../../results/parent-pref-nogenetics_${i} --verbose --same-base -p 4
-			./ARTs --courter --no-genetics --independent-pref --parent -b ../../results/parent-courter-pref-nogenetics_${i} --verbose --same-base -p 4
-		fi
-		if [ "$FDS_PREF" = true ]; then
-			./ARTs --courter --no-genetics --freq-dependent-preference -b ../../results/courter-nogenetics-nfds_${i} --verbose -p 4
-			./ARTs --parent --no-genetics --freq-dependent-preference -b ../../results/parent-nogenetics-nfds_${i} --verbose -p 4
-			./ARTs --courter --no-genetics --parent --freq-dependent-preference -b ../../results/parent-courter-nogenetics-nfds_${i} --verbose -p 4
-		fi
-	done 
-	
-fi 
+if [ "$iters" = 1 ]; then
+    if [ "$NO_GENETICS" = true ]; then
 
-#Random traits
-if [ "$CONDITIONAL" = true ]; then
-	
-	for i in `seq 1 $NUMREPS`; do
-		./ARTs --courter-conditional -b ../../results/courter-conditional_${i} --same-base -p 4
-		./ARTs --parent-conditional -b ../../results/parent-conditional_${i} --same-base -p 4
-		./ARTs --courter-conditional --parent-conditional -b ../../results/parent-courter-conditional_${i} --same-base -p 4
-	done 
+        for i in `seq ${NUMREPS}`; do
+            ./ARTs --courter --no-genetics -b ../../results/courter_unlinked_${i} --verbose --same-base -p 4
+            ./ARTs --parent --no-genetics -b ../../results/parent_unlinked_${i} --verbose --same-base -p 4
+            ./ARTs --courter --no-genetics --parent -b ../../results/parent-courter_unlinked_${i} --verbose --same-base -p 4
+            if [ "$INDEP_PREF" = true ]; then
+                ./ARTs --courter --no-genetics --independent-pref -b ../../results/courter-pref-nogenetics_${i} --verbose --same-base -p 4
+                ./ARTs --parent --no-genetics --independent-pref -b ../../results/parent-pref-nogenetics_${i} --verbose --same-base -p 4
+                ./ARTs --courter --no-genetics --independent-pref --parent -b ../../results/parent-courter-pref-nogenetics_${i} --verbose --same-base -p 4
+            fi
+            if [ "$FDS_PREF" = true ]; then
+                ./ARTs --courter --no-genetics --freq-dependent-preference -b ../../results/courter-nogenetics-nfds_${i} --verbose -p 4
+                ./ARTs --parent --no-genetics --freq-dependent-preference -b ../../results/parent-nogenetics-nfds_${i} --verbose -p 4
+                ./ARTs --courter --no-genetics --parent --freq-dependent-preference -b ../../results/parent-courter-nogenetics-nfds_${i} --verbose -p 4
+            fi
+        done
 
-fi 
+    fi
+fi
 
-#Frequency dependent selection
-if [ "$COND_NFDS" = true ]; then
+    #Random traits
+if [ "$iters" = 2 ]; then
+    if [ "$CONDITIONAL" = true ]; then
 
-	for i in `seq 1 $NUMREPS`; do
-		./ARTs --courter-conditional --freq-dependent-preference -b ../../results/courter-conditional_nfds_${i} --same-base -p 4
-		./ARTs --parent-conditional --freq-dependent-preference -b ../../results/parent-conditional_nfds_${i} --same-base -p 4
-		./ARTs --courter-conditional --parent-conditional --freq-dependent-preference -b ../../results/parent-courter-conditional_nfds_${i} --same-base -p 4
-	done >> ../../logs/002_CONDNFDS_${DATE}.log 2>1
+        for i in `seq 1 $NUMREPS`; do
+            ./ARTs --courter-conditional -b ../../results/courter-conditional_${i} --same-base -p 4
+            ./ARTs --parent-conditional -b ../../results/parent-conditional_${i} --same-base -p 4
+            ./ARTs --courter-conditional --parent-conditional -b ../../results/parent-courter-conditional_${i} --same-base -p 4
+        done
 
-fi 
+    fi
+fi
+    #Frequency dependent selection
+if [ "$iters" = 3 ]; then
+    if [ "$COND_NFDS" = true ]; then
 
-#with a genetic architecture
-if [ "$GENETIC_ARCH" = true ]; then
+        for i in `seq 1 $NUMREPS`; do
+            ./ARTs --courter-conditional --freq-dependent-preference -b ../../results/courter-conditional_nfds_${i} --same-base -p 4
+            ./ARTs --parent-conditional --freq-dependent-preference -b ../../results/parent-conditional_nfds_${i} --same-base -p 4
+            ./ARTs --courter-conditional --parent-conditional --freq-dependent-preference -b ../../results/parent-courter-conditional_nfds_${i} --same-base -p 4
+        done >> ../../logs/002_CONDNFDS_${DATE}.log 2>1
 
-	for i in `seq 1 $NUMREPS`; do
-		./ARTs --courter -b ../../results/courter_linked_${i} --verbose --same-base -p 4
-		./ARTs --parent -b ../../results/parent_linked_${i} --verbose --same-base -p 4
-		./ARTs --courter --parent -b ../../results/parent-courter_linked_${i} --verbose --same-base -p 4
-		if [ "$FDS_PREF" = true ]; then
-			./ARTs --courter --freq-dependent-preference -b ../../results/courter_nfds_${i} --verbose -p 4
-			./ARTs --parent --freq-dependent-preference -b ../../results/parent_nfds_${i} --verbose -p 4
-			./ARTs --courter --parent --freq-dependent-preference -b ../../results/parent-courter_nfds_${i} --verbose -p 4
-		fi
-		if [ "$INDEP_PREF" = true ]; then
-			./ARTs --courter --independent-pref -b ../../results/courter-pref_${i} --verbose -p 4
-			./ARTs --parent --independent-pref -b ../../results/parent-pref_${i} --verbose -p 4
-			./ARTs --courter --independent-pref --parent -b ../../results/parent-courter-pref_${i} --verbose -p 4
-		fi
-	done 
+    fi
+fi
+    #with a genetic architecture
+if [ "$iters" = 4 ]; then
+    if [ "$GENETIC_ARCH" = true ]; then
 
-fi 
+        for i in `seq 1 $NUMREPS`; do
+            ./ARTs --courter -b ../../results/courter_linked_${i} --verbose --same-base -p 4
+            ./ARTs --parent -b ../../results/parent_linked_${i} --verbose --same-base -p 4
+            ./ARTs --courter --parent -b ../../results/parent-courter_linked_${i} --verbose --same-base -p 4
+            if [ "$FDS_PREF" = true ]; then
+                ./ARTs --courter --freq-dependent-preference -b ../../results/courter_nfds_${i} --verbose -p 4
+                ./ARTs --parent --freq-dependent-preference -b ../../results/parent_nfds_${i} --verbose -p 4
+                ./ARTs --courter --parent --freq-dependent-preference -b ../../results/parent-courter_nfds_${i} --verbose -p 4
+            fi
+            if [ "$INDEP_PREF" = true ]; then
+                ./ARTs --courter --independent-pref -b ../../results/courter-pref_${i} --verbose -p 4
+                ./ARTs --parent --independent-pref -b ../../results/parent-pref_${i} --verbose -p 4
+                ./ARTs --courter --independent-pref --parent -b ../../results/parent-courter-pref_${i} --verbose -p 4
+            fi
+        done
 
-#with a genetic architecture
-if [ "$SUPERGENE" = true ]; then
+    fi
+fi
+    #with a genetic architecture
+if [ "$iters" = 5 ]; then
+    if [ "$SUPERGENE" = true ]; then
 
-	for i in `seq 1 $NUMREPS`; do
-		./ARTs --courter --supergene -b ../../results/courter_supergene_${i} --verbose --same-base -p 4
-		./ARTs --parent --supergene -b ../../results/parent_supergene_${i} --verbose --same-base -p 4
-		./ARTs --courter --parent --supergene -b ../../results/parent-courter_supergene_${i} --verbose --same-base -p 4
-		if [ "$FDS_PREF" = true ]; then
-			./ARTs --courter --supergene --freq-dependent-preference -b ../../results/courter_supergene_nfds_${i} --verbose -p 4
-			./ARTs --parent --supergene --freq-dependent-preference -b ../../results/parent_supergene_nfds_${i} --verbose -p 4
-			./ARTs --courter --parent --supergene --freq-dependent-preference -b ../../results/parent-courter_supergene_nfds_${i} --verbose -p 4
-		fi
-		if [ "$INDEP_PREF" = true ]; then
-			./ARTs --courter --independent-pref --supergene -b ../../results/courter-pref_supergene_${i} --verbose -p 4
-			./ARTs --parent --independent-pref --supergene -b ../../results/parent-pref_supergene_${i} --verbose -p 4
-			./ARTs --courter --independent-pref --supergene --parent -b ../../results/parent-courter-pref_supergene_${i} --verbose -p 4
-		fi
-	done 
-fi 
+        for i in `seq 1 $NUMREPS`; do
+            ./ARTs --courter --supergene -b ../../results/courter_supergene_${i} --verbose --same-base -p 4
+            ./ARTs --parent --supergene -b ../../results/parent_supergene_${i} --verbose --same-base -p 4
+            ./ARTs --courter --parent --supergene -b ../../results/parent-courter_supergene_${i} --verbose --same-base -p 4
+            if [ "$FDS_PREF" = true ]; then
+                ./ARTs --courter --supergene --freq-dependent-preference -b ../../results/courter_supergene_nfds_${i} --verbose -p 4
+                ./ARTs --parent --supergene --freq-dependent-preference -b ../../results/parent_supergene_nfds_${i} --verbose -p 4
+                ./ARTs --courter --parent --supergene --freq-dependent-preference -b ../../results/parent-courter_supergene_nfds_${i} --verbose -p 4
+            fi
+            if [ "$INDEP_PREF" = true ]; then
+                ./ARTs --courter --independent-pref --supergene -b ../../results/courter-pref_supergene_${i} --verbose -p 4
+                ./ARTs --parent --independent-pref --supergene -b ../../results/parent-pref_supergene_${i} --verbose -p 4
+                ./ARTs --courter --independent-pref --supergene --parent -b ../../results/parent-courter-pref_supergene_${i} --verbose -p 4
+            fi
+        done
+    fi
+fi
+    #Evolving thresholds
+if [ "$iters" = 6 ]; then
+    if [ "$EVOLVING" = true ]; then
 
-#Evolving thresholds
-if [ "$EVOLVING" = true ]; then
-	
-	for i in `seq 1 $NUMREPS`; do
-		./ARTs --courter-conditional --thresholds-evolve -b ../../results/courter-conditional_thresholds_${i} --same-base -p 4
-		./ARTs --parent-conditional --thresholds-evolve -b ../../results/parent-conditional_thresholds_${i} --same-base -p 4
-		./ARTs --courter-conditional --parent-conditional --thresholds-evolve -b ../../results/parent-courter-conditional_thresholds_${i} --same-base -p 4
-		 
-		./ARTs --courter --thresholds-evolve -b ../../results/courter_thresholds_${i} --same-base -p 4
-		./ARTs --parent --thresholds-evolve -b ../../results/parent_thresholds_${i} --same-base -p 4
-		./ARTs --courter --parent --thresholds-evolve -b ../../results/parent-courter_thresholds_${i} --same-base -p 4
-	done 
-fi 
+        for i in `seq 1 $NUMREPS`; do
+            ./ARTs --courter-conditional --thresholds-evolve -b ../../results/courter-conditional_thresholds_${i} --same-base -p 4
+            ./ARTs --parent-conditional --thresholds-evolve -b ../../results/parent-conditional_thresholds_${i} --same-base -p 4
+            ./ARTs --courter-conditional --parent-conditional --thresholds-evolve -b ../../results/parent-courter-conditional_thresholds_${i} --same-base -p 4
 
+            ./ARTs --courter --thresholds-evolve -b ../../results/courter_thresholds_${i} --same-base -p 4
+            ./ARTs --parent --thresholds-evolve -b ../../results/parent_thresholds_${i} --same-base -p 4
+            ./ARTs --courter --parent --thresholds-evolve -b ../../results/parent-courter_thresholds_${i} --same-base -p 4
+        done
+    fi
+fi
+done
 
 # generate the report
 #Rscript -e "rmarkdown::render('../../docs/002_expectationTests.Rmd')"
