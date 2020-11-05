45b8d0813969be7ecfdfb4f991d313b6f8a3b660
spflanagan <spflanagan.phd@gmail.com>
2020-11-06 08:37:28 +1300

    using gnu parallel to run the commands rather than just sending them to the background

--- scripts/100_single-locus.sh
+++ scripts/100_single-locus.sh
@@ -4,9 +4,8 @@
 ## and to ensure that all of the processes and outputs are working properly.
 
 ### --- TO RUN THE SCRIPT --- ###
+# simply run this script - it reliees on gnu parallel to spawn these jobs
 
-# in ARTs/
-#  nohup ./scripts/104_sensitivity-analysis.sh > ./logs/104_DDMMYY.log 2>&1 &
 
 
 ###----DETERMINE WHAT SHOULD RUN----###
@@ -24,26 +23,23 @@ PROGDIR="../programs/ARTs"
 cd $DIR
 cd $PROGDIR
 
-DATE=`date +%Y%m%d`
-
-
 
 ### --- RUN THE PARAMETER COMBINATIONS --- ###
 
 for i in `seq ${NUMREPS}`; do
 
     # with polygyny
-	./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_polygyny_nestBinary_courterRSadvantage_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 --viability --same-base -p 4 --polygyny &
-	./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_polygyny_nestBinary_equalFitness_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 -prs 4 -nprs 4 -crs 4 -ncrs 4 -sperm-r 1 --viability --same-base -p 4 --polygyny &
-	./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_polygyny_nestBinary_onlySpermComp_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 -prs 4 -nprs 4 -crs 4 -ncrs 4 --viability --same-base -p 4 --polygyny &
-	./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_polygyny_nestBinary_sneakRSadvantage_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 -prs 4 -nprs 8 -crs 4 -ncrs 4 --viability --same-base -p 4 --polygyny &
-	./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_polygyny_nestBinary_sneakRSadvantage_courterRSadvantage_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 -prs 4 -nprs 8 -crs 8 -ncrs 4 --viability --same-base -p 4 --polygyny &
+	echo "./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_polygyny_nestBinary_courterRSadvantage_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 --viability --same-base -p 4 --polygyny" >> "parallel_cmds.sh"
+	echo "./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_polygyny_nestBinary_equalFitness_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 -prs 4 -nprs 4 -crs 4 -ncrs 4 -sperm-r 1 --viability --same-base -p 4 --polygyny" >> "parallel_cmds.sh"
+	echo "./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_polygyny_nestBinary_onlySpermComp_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 -prs 4 -nprs 4 -crs 4 -ncrs 4 --viability --same-base -p 4 --polygyny" >> "parallel_cmds.sh"
+	echo "./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_polygyny_nestBinary_sneakRSadvantage_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 -prs 4 -nprs 8 -crs 4 -ncrs 4 --viability --same-base -p 4 --polygyny" >> "parallel_cmds.sh"
+	echo "./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_polygyny_nestBinary_sneakRSadvantage_courterRSadvantage_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 -prs 4 -nprs 8 -crs 8 -ncrs 4 --viability --same-base -p 4 --polygyny" >> "parallel_cmds.sh"
 	# without polygyny
-	./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_nestBinary_courterRSadvantage_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 --viability --same-base -p 4 &
-	./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_nestBinary_equalFitness_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 -prs 4 -nprs 4 -crs 4 -ncrs 4 -sperm-r 1 --viability --same-base -p 4 &
-	./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_nestBinary_onlySpermComp_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 -prs 4 -nprs 4 -crs 4 -ncrs 4 --viability --same-base -p 4 &
-	./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_nestBinary_sneakRSadvantage_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 -prs 4 -nprs 8 -crs 4 -ncrs 4 --viability --same-base -p 4 &
-	./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_nestBinary_sneakRSadvantage_courterRSadvantage_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 -prs 4 -nprs 8 -crs 8 -ncrs 4 --viability --same-base -p 4 &
-    wait
-
+	echo "./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_nestBinary_courterRSadvantage_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 --viability --same-base -p 4" >> "parallel_cmds.sh"
+	echo "./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_nestBinary_equalFitness_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 -prs 4 -nprs 4 -crs 4 -ncrs 4 -sperm-r 1 --viability --same-base -p 4" >> "parallel_cmds.sh"
+	echo "./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_nestBinary_onlySpermComp_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 -prs 4 -nprs 4 -crs 4 -ncrs 4 --viability --same-base -p 4" >> "parallel_cmds.sh"
+	echo "./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_nestBinary_sneakRSadvantage_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 -prs 4 -nprs 8 -crs 4 -ncrs 4 --viability --same-base -p 4" >> "parallel_cmds.sh"
+	echo "./ARTs --courter --no-genetics --parent -b ../../results/single_locus/pcu_1locus_nestBinary_sneakRSadvantage_courterRSadvantage_${i} --verbose -q 1 -x 1 -c 1 -surv-noparent 0 -surv-parent 1 -prs 4 -nprs 8 -crs 8 -ncrs 4 --viability --same-base -p 4" >> "parallel_cmds.sh"
 done
+
+parallel < "parallel_cmds.sh"
