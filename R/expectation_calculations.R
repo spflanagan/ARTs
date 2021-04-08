bba893fc1acd1ce0865c2e022932c6fa0aab4c2b
spflanagan <spflanagan.phd@gmail.com>
2021-04-08 15:35:04 +1200

    adding an appropriate contour plot

--- R/expectation_calculations.R
+++ R/expectation_calculations.R
@@ -17,3 +17,20 @@ saveRDS(expectations_list,"../results/expectations_list.RDS")
 
 # plot the expectated RS values
 library(plotly)
+
+# for NP = 0 and NS =0
+sub_calcs<-expectations_list[which(expectations_list$NP_freq==0 & expectations_list$NS_freq==0),]
+fig <- plot_ly(
+  x = sub_calcs$CP_freq, 
+  y = sub_calcs$CS_freq, 
+  z = as.matrix(sub_calcs[,c("CP_rs","CS_rs")]), 
+  type = "contour"
+)
+# add axis labels
+x<-list(title="Courter-Parent frequency")
+y<-list(title="Courter-Sneaker frequency")
+fig <- fig %>% layout(xaxis=x,yaxis=y)
+# add label to contour names
+fig <- fig %>% colorbar(title = "Relative RS")
+# plot
+fig
