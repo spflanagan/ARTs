d24c6a98a07e87fd923f66cad2bf909162137b50
Sarah <spflanagan.phd@gmail.com>
2021-07-21 10:28:45 +1200

    wrapping the creation and writing of a frequency list and initial generations into a if/then statement based on one of the true/false flags

--- morph_predictions/expectation_calculations.R
+++ morph_predictions/expectation_calculations.R
@@ -9,45 +9,26 @@ init<-FALSE
 testing<-FALSE
 create_outputs<-TRUE
 
-# create all of the intersections
-freqs_list<-expand.grid(CP=seq(0,1,0.05),
-                        CS=seq(0,1,0.05),
-                        NP=seq(0,1,0.05), 
-                        NS=seq(0,1,0.05))
-freqs_list<-freqs_list[rowSums(freqs_list)==1,]
-
-# calculate the expected values for each combination
-expectations_list<-do.call(rbind,apply(freqs_list,1,morph_predictions))
-
-saveRDS(expectations_list,"../results/expectations_list.RDS")
-
-# plot the expectated RS values
-library(plotly)
-
-# for NP = 0 and NS =0
-sub_calcs<-expectations_list[which(expectations_list$NP_freq==0 & expectations_list$NS_freq==0),]
-fig <- plot_ly(
-  x = sub_calcs$CP_freq, 
-  y = sub_calcs$CS_freq, 
-  z = as.matrix(sub_calcs[,c("CP_rs","CS_rs")]), 
-  type = "contour"
-)
-# add axis labels
-x<-list(title="Courter-Parent frequency")
-y<-list(title="Courter-Sneaker frequency")
-fig <- fig %>% layout(xaxis=x,yaxis=y)
-# add label to contour names
-fig <- fig %>% colorbar(title = "Relative RS")
-# plot
-fig
-
-
-# looping
-
-output<-data.frame(matrix(nrow=11,ncol=4))
-output[1,]<-c(CP=0.25,CS=0.25,NP=0.25,NS=0.25)
-for (i in 1:11){
-  output[i+1,]<-morph_predictions(output[i,])
+if(isTRUE(init)){
+  # create all of the intersections
+  freqs_list<-expand.grid(CP=seq(0,1,0.05),
+                          CS=seq(0,1,0.05),
+                          NP=seq(0,1,0.05), 
+                          NS=seq(0,1,0.05))
+  freqs_list<-freqs_list[rowSums(freqs_list)==1,]
+  
+  write.table(freqs_list,"freqs_list.txt",col.names=TRUE,row.names=TRUE)
+  
+  # create some expectations
+  expectations_list<-do.call(rbind,apply(freqs_list,1,morph_gens,gens=100)) # this is slow
+  
+  # saved on 25 May 2021 to save time for defaults.
+  saveRDS(expectations_list,"expectations_100gens.RDS")
+  
+  
+  
+} else{
+  freqs_list<-read.delim("freqs_list.txt",header=TRUE)
 }
 
 
