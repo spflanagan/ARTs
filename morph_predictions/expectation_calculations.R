cda155b0f9945e009360e51fa55a3d2ea9fed7a5
Sarah <spflanagan.phd@gmail.com>
2021-07-21 10:31:10 +1200

    wrapping the figure plotting tests into an if/then loop AND putting together a create_outputs if/then loop to run a bunch of parameter combos

--- morph_predictions/expectation_calculations.R
+++ morph_predictions/expectation_calculations.R
@@ -32,3 +32,63 @@ if(isTRUE(init)){
 }
 
 
+if(isTRUE(testing)){
+  
+  # calculate the expected values for each combination - one generation
+  expectations_list<-do.call(rbind,apply(freqs_list,1,morph_predictions))
+  
+  
+  # plot the expectated RS values
+  library(plotly)
+  
+  # for NP = 0 and NS =0
+  sub_calcs<-expectations_list[which(expectations_list$NP_freq==0 & expectations_list$NS_freq==0),]
+  fig <- plot_ly(
+    x = sub_calcs$CP_freq, 
+    y = sub_calcs$CS_freq, 
+    z = as.matrix(sub_calcs[,c("CP_rs","CS_rs")]), 
+    type = "contour"
+  )
+  # add axis labels
+  x<-list(title="Courter-Parent frequency")
+  y<-list(title="Courter-Sneaker frequency")
+  fig <- fig %>% layout(xaxis=x,yaxis=y)
+  # add label to contour names
+  fig <- fig %>% colorbar(title = "Relative RS")
+  # plot
+  fig
+  
+  # testing the case where the pop goes extinct.
+  morph_gens(gens=100,freqs=freqs_list[21,])
+  morph_gens(gens=100,freqs=c(0.25,0.25,0.25,0.25))
+}
+
+
+if(isTRUE(create_outputs)){
+  # create a HUGE data.frame with the different outputs, 
+  # so that they can be loaded into the shiny app.
+  rs<-seq(0.11,2.11,0.1)
+  cs<-seq(0,1,0.25)
+  
+  morph_results<-as.data.frame(matrix(ncol=10,nrow=0))
+  colnames(morph_results)<-c("initial_CP","initial_CS","initial_NP","initial_NS",
+                             "CP","CS","NP","NS","r","c")
+  
+  expectations_list<-dplyr::bind_rows(apply(freqs_list,1,morph_gens,gens=100, r=0.11,c=1))
+  
+  for(r in rs){
+    for(c in cs){
+      outputs<-dplyr::bind_rows(apply(freqs_list,1,morph_gens,gens=100, r=r,c=c))
+      to_save<-dplyr::bind_cols(freqs_list,outputs,.name_repair = "minimal")
+      colnames(to_save)[1:4]<-paste0("initial_",colnames(to_save)[1:4])
+      to_save$r<-r
+      to_save$c<-c
+      morph_results<-dplyr::bind_rows(morph_results,to_save)
+    }
+  }
+  
+  saveRDS(morph_results,"morph_results.RDS")
+}
+
+
+
