fa6b7196923e676573e410daa9834374b965b1de
spflanagan <sflanagan19@gmail.com>
2021-09-16 16:36:42 +1200

    updating names to no longer have sneakers

--- morph_predictions/check_freqs.R
+++ morph_predictions/check_freqs.R
@@ -1,5 +1,5 @@
 #' Function to check frequency inputs
-#' @param freqs A vector containing the frequencies of the other morphs (CP,CS,NP,NS). 
+#' @param freqs A vector containing the frequencies of the other morphs (CP,CN,NP,NN). 
 #'              The frequencies must be labelled or all four provided in the order above.
 #'              If it is a vector with three values, the fourth is calculated as the frequency of the morph of interest.
 #' @return Returns the checked/corrected vector of allele frequencies
@@ -7,22 +7,22 @@ check_freqs<-function(freqs){
   # sanity checks of freqs
   if(length(freqs)<4){
     if(length(freqs)==3){
-      if(length(which((names(freqs) %in% c("CP","CS","NP"))==TRUE))==3){
-        freqs["NS"]<-1-sum(freqs)
-      } else if (length(which((names(freqs) %in% c("CP","CS","NS"))==TRUE))==3){
+      if(length(which((names(freqs) %in% c("CP","CN","NP"))==TRUE))==3){
+        freqs["NN"]<-1-sum(freqs)
+      } else if (length(which((names(freqs) %in% c("CP","CN","NN"))==TRUE))==3){
         freqs["NP"]<-1-sum(freqs)
-      } else if (length(which((names(freqs) %in% c("CP","NP","NS"))==TRUE))==3){
-        freqs["CS"]<-1-sum(freqs)
-      } else if (length(which((names(freqs) %in% c("CS","NP","NS"))==TRUE))==3){
+      } else if (length(which((names(freqs) %in% c("CP","NP","NN"))==TRUE))==3){
+        freqs["CN"]<-1-sum(freqs)
+      } else if (length(which((names(freqs) %in% c("CN","NP","NN"))==TRUE))==3){
         freqs["CP"]<-1-sum(freqs)
       }
     }else{
       stop("Too few frequencies were provided to the function.")
     }
   } else{
-    if(length(which((names(freqs) %in% c("CP","CS","NP","NS"))==TRUE))<4){
-      names(freqs)<- c("CP","CS","NP","NS")
-      warning("The given frequencies have been renamed in the order CP, CS, NP, and NS.")
+    if(length(which((names(freqs) %in% c("CP","CN","NP","NN"))==TRUE))<4){
+      names(freqs)<- c("CP","CN","NP","NN")
+      warning("The given frequencies have been renamed in the order CP, CN, NP, and NN.")
     }  
   }
   if(round(sum(freqs,na.rm=TRUE)) != 1){
