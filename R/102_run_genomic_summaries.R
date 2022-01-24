cc403a92113e29166411ec0e2072a2b603787f04
Sarah <spflanagan.phd@gmail.com>
2022-01-25 10:23:19 +1300

    dividing up the R functions to deal with both QTLs and SUPs

--- R/102_run_genomic_summaries.R
+++ R/102_run_genomic_summaries.R
@@ -4,12 +4,28 @@ library(dplyr)
 setwd("../results/")
 source("../R/003_genomic_summaries.R")
 
-path <- "/mnt/BigData/fixedART-results/qtls"
-qtl_outliers<-dplyr::bind_rows(lapply(
-  list.files(path=path, 
-             pattern = "*.vcf",
-             full.names = TRUE),
-  get_summary,
-  path = path))
-write.csv(qtl_outliers,"qtl_outliers.csv",quote=FALSE,row.names = FALSE)
+QTLS<-FALSE
+SUPS<-TRUE
+if(isTRUE(QTLS)){
+  path <- "/mnt/BigData/fixedART-results/qtls"
+  qtl_outliers<-dplyr::bind_rows(lapply(
+    list.files(path=path, 
+               pattern = "*.vcf",
+               full.names = TRUE),
+    get_summary,
+    path = path))
+  write.csv(qtl_outliers,"qtl_outliers.csv",quote=FALSE,row.names = FALSE)
+  
+}
 
+if(isTRUE(SUPS)){
+  path <- "/mnt/BigData/fixedART-results/supergene/"
+  supergene_outliers<-dplyr::bind_rows(lapply(
+    list.files(pattern="*.vcf",
+               path= path, 
+               full.names = TRUE),
+    get_summary,
+    path = path))
+  write.csv(supergene_outliers,"supergene_outliers.csv",quote=FALSE,row.names = FALSE)
+  
+}
