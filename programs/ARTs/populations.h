9ec6952735f50d68387d9502f43ae7bd4424539a
Sarah P. Flanagan <spflanagan@users.noreply.github.com>
2022-03-10 20:22:09 +1300

    reverting moving rs to babies made change

--- programs/ARTs/populations.h
+++ programs/ARTs/populations.h
@@ -2271,7 +2271,6 @@ public:
 		{
 			nb = making_babies(gp, fecundity[k], num_progeny, this_nest.mom, this_nest.all_dads[k]);
 			off_counter = off_counter+ nb;
-			adults[this_nest.all_dads[k]].lifetime_rs = adults[this_nest.all_dads[k]].lifetime_rs + fecundity[k];
 		}
 		
 		//sanity check - these should be the same
@@ -2379,6 +2378,8 @@ public:
 			// // record the males' mating success
 			for(k = 0; k < fecundity_share.size(); k++)
 			{
+
+				adults[male_ids[k]].lifetime_rs = adults[male_ids[k]].lifetime_rs + (fecundity_share[k]*adults[fem_id].pot_rs);
 
 				adults[male_ids[k]].pot_rs = adults[male_ids[k]].pot_rs - (fecundity_share[k]*adults[fem_id].pot_rs);
 				if(adults[male_ids[k]].pot_rs < 0) adults[male_ids[k]].pot_rs = 0;
