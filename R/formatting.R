1abbbcc40a9be7942feeec3b51c634b0f613ab98
Sarah P. Flanagan <spflanagan@users.noreply.github.com>
2023-01-16 15:28:59 +1300

    creating a script to provide consistent formatting

---
+++ R/formatting.R
@@ -0,0 +1,10 @@
+
+
+cols<-c(courter='#4daf4a',parent='#377eb8',cpref="#b2df8a",ppref="#a6cee3")
+cols2<-c(CP='#e41a1c',NCP='#ff7f00',CNP='#ffff33',NCNP='#a65628')
+base_keep_cols<- c("Pop","PopSize","NumMal","NumFem","ParentThresh","ParentFreq","CourterThresh","CourterFreq","FreqNcNp","FreqCNp",
+                   "FreqNcP","FreqCP","Courter2NonRS","Parent2NonSurvival")
+poly_keep_cols<- c("Pop","PopSize","NumMal","NumFem","ParentThresh","ParentFreq","CourterThresh","CourterFreq","FreqNcNp","FreqCNp",
+                   "FreqNcP","FreqCP","Polygyny")
+divcols<-c(low='#f781bf',high='#984ea3')
+
